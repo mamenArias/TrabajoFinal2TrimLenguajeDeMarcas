bd3fe922e034b6a2be6d4ad0f354750d83a4fcb2
Mª Carmen Arias <mcariasdeharo@gmail.com>
2021-03-16 17:38:35 +0100

    Supr jQuery
    Se elimina código de jQuery inservible

--- js/proyecto2Trimestre.js
+++ js/proyecto2Trimestre.js
@@ -12,19 +12,3 @@ function actualizar() {
 
 $("#bc").text("Inicio");
 $("#bc2").text("Actualidad");
-
-/*
-nombre();
-
-function nombre() {
-    $(".migas").text(sessionStorage.getItem(onclick()));
-}
-
-$(function() {
-    $(".lado ul li a").click(function() {
-        // quitar .seleccionado a todos (por si hay alguno)
-        $(".lado ul li a").removeClass("activo");
-        // agregar seleccionado a "este" elemento.
-        $(this).addClass("activo");
-    });
-});*/
